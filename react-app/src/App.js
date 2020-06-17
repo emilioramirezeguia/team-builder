0134ebf9310b258f13d2a6347986179fed59bb10
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-16 21:06:38 -0700

    Create Form compoment

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { v4 as uuid } from "uuid";
 import Card from "./components/Card.jsx";
+import Form from "./components/Form.jsx";
 import './App.css';
 
 const originalTeam = [{
@@ -15,11 +16,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Meet the team!</h1>
         {
           team.map(member => {
             return <Card key={member.id} member={member} />
           })
         }
+        <Form />
       </header>
     </div>
   );
